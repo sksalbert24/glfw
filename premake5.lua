3811b8b3b22e4c060383c61857a42840cded1ac0
Sudeep Kaushal <sksalbert24@gmail.com>
2023-10-02 12:59:36 +0530

    Premake file Change

--- premake5.lua
+++ premake5.lua
@@ -7,6 +7,26 @@ project "Glfw"
 		targetdir "Build/%{cfg.buildcfg}"
 		objdir "Build/%{cfg.buildcfg}/obj"
 
+		files
+		{
+			"include/GLFW/glfw3.h",
+			"include/GLFW/glfw3native.h",
+			"src/glfw_config.h",
+			"src/context.c",
+			"src/init.c",
+			"src/input.c",
+			"src/monitor.c",
+
+			"src/null_init.c",
+			"src/null_joystick.c",
+			"src/null_monitor.c",
+			"src/null_window.c",
+
+			"src/platform.c",
+			"src/vulkan.c",
+			"src/window.c",
+		}
+
 		filter "system:windows"
 			systemversion "latest"
 
